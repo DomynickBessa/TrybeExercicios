a3e4975530d4bb0592e419bc1f10ee127cbea0e3
Domynick Bessa <nyckinha14@gmail.com>
2023-04-04 13:57:16 -0300

    exercício monitoria4abril

---
+++ monitoria4abril.js
@@ -0,0 +1,45 @@
+// Exercício 1 - Crie uma função que calcule a área e o perímetro de um quadrilátero qualquer.
+// Essa função deve receber dois parâmetros: base e altura e retornar um objeto no formato:
+// Fórmulas: perimetro = (2 * base) + (2 * altura) area = base * altura
+
+let objeto = {
+    area: 0,
+    perimetro: 0,
+  };
+  function calculaAreaEPerimetro(base, altura){
+    return objeto = {
+        area: base * altura,
+        perimetro: (2 * base) + (2 * altura)
+    };
+  }
+
+  
+  // Exercício 2 - Crie uma função que, dado um array de números inteiros,
+  // retorne a quantidade de números pares e ímpares no formato:
+  
+  let numeros = {
+    pares: 0,
+    ímpares: 0,
+  };
+  function quantidadeParImpar(arrayOfNumbers) {
+    let pares = 0;
+    let impares =0;
+   // for (let index = 0; index.lenght; index +=1)
+   return {
+    
+   }
+
+
+
+  }
+  
+  // Exercício 3 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word.
+  // Considere que a string ending sempre será menor que a string word.
+  // Dica: Use o split.
+  
+  // valor de teste: ‘trybe’ e ‘be’
+  // valor esperado no retorno da função: true
+  // verificaFimPalavra(‘trybe’, ‘be’);
+  // Retorno esperado: true
+  // verificaFimPalavra(‘joaofernando’, ‘fernan’);
+  // Retorno esperado: false;
